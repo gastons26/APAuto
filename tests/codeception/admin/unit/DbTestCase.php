54a6fb102e3e8fc31eac19f86766dfb040e17567
gastons26 <gatis.lapsins@gmail.com>
2016-05-23 11:56:57 +0300

    Project start
    Yii2 advanced template with some modification.
    Let's start development core for APAuto

---
+++ tests/codeception/admin/unit/DbTestCase.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace tests\codeception\admin\unit;
+
+class DbTestCase extends \yii\codeception\DbTestCase
+{
+    public $appConfig = '@tests/codeception/config/admin/unit.php';
+}
---
+++ tests/codeception/admin/unit/TestCase.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace tests\codeception\admin\unit;
+
+class TestCase extends \yii\codeception\TestCase
+{
+    public $appConfig = '@tests/codeception/config/admin/unit.php';
+}
