afb85dcef785c4c2c01d3b9a963aa1eba3165a4c
gastons26 <gatis.lapsins@gmail.com>
2016-05-23 13:01:23 +0300

    .gitignore removal
    Remove some unnecessary .gitignore files

---
+++ admin/config/main-local.php
@@ -0,0 +1,25 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'kcC1KPG0q2n7Mx0avWDZCmvxJHEN4-z2',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
